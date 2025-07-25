82b8f3846b10756085de52e1dfc55ca2ab8f6177
KJanaki-12 <kenchugundujanaki@gmail.com>
2025-07-25 15:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function daysOfYear(year) {
+	if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+		return 366;
+	}else{
+		return 365;
+	}
+}
+
+let year=prompt("Enter a year:");
+let days=daysOfYear(year);
+alert(`The year ${year} has ${days} days.`);
